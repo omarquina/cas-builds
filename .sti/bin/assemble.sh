bddb2d36abc3a13dace59fd4bc032361df4e7b93
root <root@nodo150.cautivatech.com>
2018-01-17 16:09:57 -0300

    obteniendo git de cas_consumes_json

--- .sti/bin/assemble.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/sh
-
-# Archivo que sobreescribiría el assemble del imagebuilder para s2i
-# es una opción para poder compilar todos el código fuente de todos los proyectos CAS, 
-# para ser agregados a Tomcat
